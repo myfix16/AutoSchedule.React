c7be3dc0a0ca245393b6bcca56290de35131e344
Luuvy <119010256@link.cuhk.edu.cn>
2021-09-18 14:55:14 +0800

    about page

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,3 +1,27 @@
 export default function () {
-    return <div>About</div>
+    return  (
+        <div className="page about">
+        <h3>About</h3>
+        <br/>
+        
+        <h4>Information</h4>
+        <p>Source code can be viewed on <a href="https://github.com/myfix16/AutoSchedule.React">GitHub</a></p>
+        
+        <h4>Found a bug? Want new features?</h4>
+        <p>
+            If you have found a bug within the app or want to suggest new features, please file an issue on GitHub Issue Page: <br/>
+            <a href="https://github.com/myfix16/AutoSchedule.React/issues/new">New Issue</a>
+        </p>
+        
+        <h4>Dependencies and References</h4>
+
+        
+        <h4>Disclaimer</h4>
+        <p>THIS CODE AND INFORMATION IS PROVIDED ‘AS IS’ WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT
+        NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.</p>
+        <p>Copyright (c) myfix16. All rights reserved.</p>
+        
+
+        </div>
+    )
 }
