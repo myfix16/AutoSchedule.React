f9afdd0352a6fc360585ad54c216cbb4624b1b4e
Luuvy <119010256@link.cuhk.edu.cn>
2021-09-20 09:08:54 +0800

    homepage draft

---
+++ src/components/PageLoading.jsx
@@ -0,0 +1,9 @@
+export default function () {
+  return (
+    <div style={{textAlign: "center"}}>
+      <div style={{fontSize: "5rem"}}>Welcome to Auto Schedule</div>
+
+      <div>Arrange your academic schedule easily</div>
+    </div>
+  );
+}
--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,3 +1,35 @@
+import './index.less';
+import React from 'react';
+import { Button, Modal, List, Row, Col } from 'antd';
+import {CheckCircleOutlined} from "@ant-design/icons"
 export default function () {
-  return <div>HOME</div>;
+  const [availableCourse, setAvailableCourse] = React.useState([]);
+  const [selCourse, setSelCourse] = React.useState([]);
+  const handleAddCourses = () => {};
+  const viewAvailableCourses = () => {
+
+  }
+  return (
+    <div className="page homepage">
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col span={12} className="selected-course" style={{ borderRight: '1px solid #F0F0F0' }}>
+          {/* <div className="available-course btn">
+          </div> */}
+          <List header={<div
+           style={{display:"flex", justifyContent: "space-between"}}>
+            <span>
+              Selected Courses
+            </span>
+            <Button onClick={handleAddCourses}>Add From Available Courses</Button>
+          </div>} dataSource={selCourse} />
+        </Col>
+        <Col span={12} className="available-schedule">
+          {/* <div className="view-available-course btn">
+            <Button onClick={viewAvailableCourses}>View Available Schedule</Button>
+          </div> */}
+          <List header={<div><CheckCircleOutlined style={{marginRight: "0.5rem"}}/><span>Available Schedule</span></div>} />
+        </Col>
+      </Row>
+    </div>
+  );
 }
