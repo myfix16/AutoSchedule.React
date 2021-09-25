ef1937bd1ad30f103f6fa9746a7ae6c294d257f9
Luuvy <119010256@link.cuhk.edu.cn>
2021-09-25 21:38:17 +0800

    get schedules

--- config/proxy.js
+++ config/proxy.js
@@ -9,7 +9,7 @@
 export default {
   dev: {
     '/api/': {
-      target: 'https://preview.pro.ant.design',
+      target: 'https://api-autoschedule.azurewebsites.net',
       changeOrigin: true,
       pathRewrite: {
         '^': '',
